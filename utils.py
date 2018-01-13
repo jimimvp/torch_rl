2f1f7f37e1278257aabe107e0cb479530f90d138
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2018-01-13 13:50:46 +0100

    Removed unnecessary imports, shifted some functions to utils

--- utils.py
+++ utils.py
@@ -1,8 +1,46 @@
 import torch as tor
 from torch.autograd import Variable
 import numpy as np
+import random
+from collections import namedtuple, deque
 
 def to_tensor(ndarray, volatile=False, requires_grad=False, dtype=tor.FloatTensor):
     return Variable(
         tor.from_numpy(ndarray), volatile=volatile, requires_grad=requires_grad
     ).type(dtype)
+
+def gauss_weights_init(m, mu, std):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        m.weight.data.normal_(mu, std)
+
+
+def to_input_state_goal(state, goal):
+
+    x = np.hstack((state,goal))
+    x = np.expand_dims(x, 0)
+    return to_tensor(x)
+
+def to_numpy(out):
+    return out.data.numpy()
+
+
+
+Transition = namedtuple(
+    'Transition', ('state', 'action', 'mask', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque(maxlen=capacity)
+
+    def push(self, *args):
+        """Saves a transition."""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
