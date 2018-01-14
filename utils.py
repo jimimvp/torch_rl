5f176e36c7b97d6810dcaec5ac157e21dbe95f24
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2018-01-14 13:06:23 +0100

    Added hard update and soft update to utils

--- utils.py
+++ utils.py
@@ -34,6 +34,33 @@ Transition = namedtuple(
     'Transition', ('state', 'action', 'next_state', 'reward'))
 
 
+def soft_update(target, source, tau):
+	"""
+	Copies the parameters from source network (x) to target network (y) using the below update
+	y = TAU*x + (1 - TAU)*y
+	:param target: Target network (PyTorch)
+	:param source: Source network (PyTorch)
+	:return:
+	"""
+	for target_param, param in zip(target.parameters(), source.parameters()):
+		target_param.data.copy_(
+			target_param.data * (1.0 - tau) + param.data * tau
+		)
+
+
+def hard_update(target, source):
+	"""
+	Copies the parameters from source network to target network
+	:param target: Target network (PyTorch)
+	:param source: Source network (PyTorch)
+	:return:
+	"""
+	for target_param, param in zip(target.parameters(), source.parameters()):
+			target_param.data.copy_(param.data)
+
+
+
+
 class ReplayMemory(object):
 
     def __init__(self, capacity):
