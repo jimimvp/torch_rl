9b3a0b0f3b2d924c0d5563cf7a6a0d23be67d170
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2018-01-14 13:07:56 +0100

    Added Ornstein-Uhlenbeck process to utils

--- utils.py
+++ utils.py
@@ -4,13 +4,14 @@ import numpy as np
 import random
 from collections import namedtuple, deque
 
+
 def to_tensor(ndarray, volatile=False, requires_grad=False, dtype=tor.FloatTensor):
     return Variable(
         tor.from_numpy(ndarray), volatile=volatile, requires_grad=requires_grad
     ).type(dtype)
 
-def gauss_weights_init(mu, std):
 
+def gauss_weights_init(mu, std):
     def init(m):
         classname = m.__class__.__name__
         if classname.find('Linear') != -1:
@@ -20,14 +21,13 @@ def gauss_weights_init(mu, std):
 
 
 def to_input_state_goal(state, goal):
-
-    x = np.hstack((state,goal))
+    x = np.hstack((state, goal))
     x = np.expand_dims(x, 0)
     return to_tensor(x)
 
+
 def to_numpy(out):
     return out.data.numpy()
-
 
 
 Transition = namedtuple(
@@ -35,34 +35,50 @@ Transition = namedtuple(
 
 
 def soft_update(target, source, tau):
-	"""
-	Copies the parameters from source network (x) to target network (y) using the below update
-	y = TAU*x + (1 - TAU)*y
-	:param target: Target network (PyTorch)
-	:param source: Source network (PyTorch)
-	:return:
-	"""
-	for target_param, param in zip(target.parameters(), source.parameters()):
-		target_param.data.copy_(
-			target_param.data * (1.0 - tau) + param.data * tau
-		)
+    """
+    Copies the parameters from source network (x) to target network (y) using the below update
+    y = TAU*x + (1 - TAU)*y
+    :param target: Target network (PyTorch)
+    :param source: Source network (PyTorch)
+    :return:
+    """
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(
+            target_param.data * (1.0 - tau) + param.data * tau
+        )
 
 
 def hard_update(target, source):
-	"""
-	Copies the parameters from source network to target network
-	:param target: Target network (PyTorch)
-	:param source: Source network (PyTorch)
-	:return:
-	"""
-	for target_param, param in zip(target.parameters(), source.parameters()):
-			target_param.data.copy_(param.data)
+    """
+    Copies the parameters from source network to target network
+    :param target: Target network (PyTorch)
+    :param source: Source network (PyTorch)
+    :return:
+    """
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
 
 
+# Based on http://math.stackexchange.com/questions/1287634/implementing-ornstein-uhlenbeck-in-matlab
+class OrnsteinUhlenbeckActionNoise:
+    def __init__(self, action_dim, mu=0, theta=0.15, sigma=0.2):
+        self.action_dim = action_dim
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+        self.X = np.ones(self.action_dim) * self.mu
+
+    def reset(self):
+        self.X = np.ones(self.action_dim) * self.mu
+
+    def sample(self):
+        dx = self.theta * (self.mu - self.X)
+        dx = dx + self.sigma * np.random.randn(len(self.X))
+        self.X = self.X + dx
+        return self.X
 
 
 class ReplayMemory(object):
-
     def __init__(self, capacity):
         self.memory = deque(maxlen=capacity)
 
