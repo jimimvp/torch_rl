6b324f8002a6db8816f3634aeca2f7045b35e5ca
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2018-01-29 13:59:17 +0100

    Added reservoir tests

---
+++ torch_rl/tests/reservoir_tests.py
@@ -0,0 +1,18 @@
+import gym
+from torch_rl.models import Reservoir
+import numpy as np
+
+
+env = gym.make("Pendulum-v0")
+state = env.reset()
+reservoir = Reservoir(0.4, 0.01, spectral_radius=0.9, network_size=200, input_size=env.observation_space.shape[0])
+
+state_prev = np.zeros(200)
+
+for i in range(100):
+
+    state, reward, done, _ = env.step(env.action_space.sample())
+    print(state)
+    state = reservoir.forward(state)
+    print(state - state_prev)
+    state_prev = state
