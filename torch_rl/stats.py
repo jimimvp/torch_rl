c4005f408a9dcc77e2aedfbc2b876b87d4641055
Marin Vlastelica Pogancic <marin.vlastelicap@gmail.com>
2018-02-02 13:01:55 +0100

    Added matching of training data files when loading to *.stats and smarter output directory creation

--- torch_rl/stats.py
+++ torch_rl/stats.py
@@ -3,15 +3,16 @@ import pandas as pd
 import datetime
 import os
 import numpy as np
-
+from torch_rl.utils import Parameters, prRed
+import glob
 
 class RLTrainingStats(object):
     """
         Keeps training statistics and writes them to a file and loads them.
     """
     def __init__(self, episode_window=10, step_window=10,
-                 sample_rate_episodes=1, sample_rate_steps=None, save_rate = 10,
-                 save_destination=None):
+                 sample_rate_episodes=1, sample_rate_steps=None, save_rate=10,
+                 save_destination=None, hyperparameters=None):
 
         self.episode_window = episode_window
         self.episode_reward_buffer = deque(maxlen=episode_window)
@@ -22,15 +23,38 @@ class RLTrainingStats(object):
         self.rewards = []
         self.moving_average_rewards = []
         self.save_rate = save_rate
+        self.hyperparameters = hyperparameters
 
         if save_destination is None:
             self.save_destination = 'training_stats_' + str(datetime.datetime.now())
 ***REMOVED***
             self.save_destination = save_destination
 
+        if not os.path.isdir(self.save_destination):
+            os.makedirs(self.save_destination)
+***REMOVED***
+            prRed(self.save_destination + " is a directory already, delete for new training data? y/n")
+            res = input()
+            res = res.lower()
+            if res == 'y':
+                os.rmdir(self.save_destination)
+    ***REMOVED***
+                raise Exception("Start training with another save destination name.")
+
+
+        # Save hyperparameters to a file
+        self.save_hyperparameters()
+
         # Pandas data frames
         self.episode_data = None
         self.step_data = None
+
+    def save_hyperparameters(self):
+        if self.hyperparameters:
+            if not isinstance(self.hyperparameters, Parameters):
+                raise Exception("User Parameters from torch_rl.utils.Parameters to store parameters")
+            df = pd.DataFrame.from_records(self.hyperparameters.__dict__)
+            df.to_pickle(os.path.join(self.save_destination, "parameters.cfg"))
 
     def step(self, episode, step, reward,**kwargs):
         kwargs["reward"] = reward
@@ -69,8 +93,7 @@ class RLTrainingStats(object):
         path = self.save_destination
 
         print(" #Saving data to", path)
-        if not os.path.isdir(path):
-            os.makedirs(path)
+
         if not self.episode_data is None:
             name = time_stamp + "_episode.stats"
             self.episode_data.to_pickle(os.path.join(path, name))
@@ -83,7 +106,7 @@ class RLTrainingStats(object):
 
     @staticmethod
     def load(path="./training_stats"):
-        files = os.listdir(path)
+        files = glob.glob(path + "/*.stats")
         files = sorted(files)
         data = pd.read_pickle(os.path.join(path, files[0]))
 
