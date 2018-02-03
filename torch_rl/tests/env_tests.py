3511fc17055ea43cd342eeeb3d45188a5943c8f9
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2018-02-03 09:50:43 +0100

    Better env tests

--- torch_rl/tests/env_tests.py
+++ torch_rl/tests/env_tests.py
@@ -1,7 +1,22 @@
 import gym
 import torch_rl.envs
+import unittest
+from unittest import TestCase
+import numpy as np
 
-bandit_env = gym.make('BanditsX2-v0')
-state = bandit_env.reset()
+class TestBanditEnv(TestCase):
 
-print(state)
+    def setUp(self):
+        self.env = gym.make('BanditsX2-v0')
+
+    def testStep(self):
+        obs, reward, done, info = self.env.step(self.env.action_space.sample())
+        self.assertTrue(not obs is None)
+        self.assertTrue(not reward is None)
+
+    def testReset(self):
+        state = self.env.reset()
+        self.assertTrue(isinstance(state, np.ndarray), "State is instance numpy array")
+        self.assertTrue(state.shape[0] == self.env.observation_space.shape[0], "State shape is equal to observation space shape")
+
+
