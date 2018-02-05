3a4d5bd9e79b55cb72bff130ba7c24f8190fe211
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2018-02-05 12:35:58 +0100

    Added environment tests migrated to pytest

--- torch_rl/tests/env_tests.py
+++ torch_rl/tests/env_tests.py
@@ -3,20 +3,25 @@ import torch_rl.envs
 import unittest
 from unittest import TestCase
 import numpy as np
+import pytest
+import sys
 
 class TestBanditEnv(TestCase):
 
-    def setUp(self):
-        self.env = gym.make('BanditsX2-v0')
+    @classmethod
+    def setup_class(cls):
+        cls.env = gym.make('BanditsX2-v0')
 
-    def testStep(self):
+    def test_step(self):
         obs, reward, done, info = self.env.step(self.env.action_space.sample())
         self.assertTrue(not obs is None)
         self.assertTrue(not reward is None)
 
-    def testReset(self):
+    def test_reset(self):
         state = self.env.reset()
         self.assertTrue(isinstance(state, np.ndarray), "State is instance numpy array")
         self.assertTrue(state.shape[0] == self.env.observation_space.shape[0], "State shape is equal to observation space shape")
 
 
+if __name__ == '__main__':
+    pytest.main([sys.argv[0]])
