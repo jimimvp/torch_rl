d975790d8c9dc0ed7f15c37b60b0d930a3ce8110
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2018-02-05 12:33:54 +0100

    Added additional test cases and migrated to pytest

--- torch_rl/tests/hindsight_replay_memory_test.py
+++ torch_rl/tests/hindsight_replay_memory_test.py
@@ -2,43 +2,57 @@ from torch_rl.memory import HindsightMemory
 import gym
 import numpy as np
 from tqdm import tqdm
+from unittest import TestCase
+import pytest
+import sys
 
-memory = HindsightMemory(1000, window_length=1)
-env = gym.make("Pendulum-v0")
-env.reset()
-num_transitions = 4
-hind_size = 8
+def nnone(obj):
+    return not obj is None
+
+class HindsightReplayTest(TestCase):
+    memory = HindsightMemory(1000, window_length=1)
+    @classmethod
+    def setup_class(cls):
+        """ setup any state specific to the execution of the given class (which
+        usually contains tests).
+        """
+        cls.episode = 1
+        for i in range(500):
+            if i % 20 == 0:
+                cls.memory.append(cls.episode, 1, 1, 1, True)
+                cls.episode += 1
+    ***REMOVED***
+                cls.memory.append(cls.episode, 1, 1, 1, False)
+    def test_append(self):
+        self.memory.append(0,1,1,1,1,False)
+
+    def test_from_same_episode(self):
+        self.assertTrue(self.episode > 1, "Episode has to be greater than 0")
+        self.assertTrue(self.memory.nb_entries > 0, "Number of entries is not greater than 0")
+        episode = self.memory.observations[0]
+        for i in range(self.memory.nb_entries-1):
+            if self.memory.observations[i] != episode:
+                episode+=1
+            self.assertTrue(self.memory.observations[i] == episode, "Episodes should come incrementally when iterating.")
+
+    def test_hindsight(self):
+        for hindsight in self.memory.hindsight_buffer:
+            self.assertTrue(not hindsight is None, "If you can iterate over it, should not be None")
+            self.assertTrue(len(hindsight) == 8, "Length of every hindsight entry should be 8")
+            # Check that all of the idxs belong to the same episode
+            episode = self.memory.observations[hindsight[0]]
+            for i in hindsight[1:]:
+                e = self.memory.observations[i]
+                print(episode, e)
+                self.assertTrue(self.memory.observations[i] == episode, "Every experience in hindsight should be from same episode")
 
 
-for i in range(1000):
-    # Warmup
-    action = env.action_space.sample()
-    goal = env.observation_space.sample()
-    observation, reward, done, _ = env.step(action)
-    reward = 1 if np.sum(np.abs(observation-goal)**2) < 1. else -1
-    if done:
-        env.reset()
-    memory.append(observation, env.observation_space.sample(), action=action, terminal=done, reward=reward)
+    def test_hindsight_pairing(self):
+        for hindsight in self.memory.hindsight_buffer:
+            for i in hindsight:
+                e = self.memory[i]
+                [self.assertTrue(nnone(obj), "Every object in experience should not be None") for obj in e]
 
 
-for i in tqdm(range(100000)):
-    action = env.action_space.sample()
-    goal = env.observation_space.sample()
-    observation, reward, done, _ = env.step(action)
-    reward = 1 if np.sum(np.abs(observation - goal) ** 2) < 1. else -1
-
-    memory.append(observation, env.observation_space.sample(), action=action, terminal=done, reward=reward)
-
-    state0_batch, goal_batch, action_batch, reward_batch, \
-    state1_batch,terminal1_batch = memory.sample_and_split(num_transitions*hind_size + num_transitions)
-
-    if done:
-        env.reset()
-
-    assert len(state0_batch) == num_transitions*hind_size+num_transitions
-
-
-
-
-
-
+if __name__ == '__main__':
+    pytest.main([sys.argv[0]])
