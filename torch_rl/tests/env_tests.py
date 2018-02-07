5fbb6373b0c58e9a7f1d127301475841b461a2a3
Marin Vlastelica Pogancic <marin.vlastelicap@gmail.com>
2018-02-07 14:59:21 +0100

    Added tests for sparse reward wrapper

--- torch_rl/tests/env_tests.py
+++ torch_rl/tests/env_tests.py
@@ -6,6 +6,8 @@ import numpy as np
 import pytest
 import sys
 
+from torch_rl.envs import SparseRewardGoalEnv
+
 class TestBanditEnv(TestCase):
 
     @classmethod
@@ -21,6 +23,26 @@ class TestBanditEnv(TestCase):
         state = self.env.reset()
         self.assertTrue(isinstance(state, np.ndarray), "State is instance numpy array")
         self.assertTrue(state.shape[0] == self.env.observation_space.shape[0], "State shape is equal to observation space shape")
+
+
+class TestSparseRewardWrapper(TestCase):
+
+    @classmethod
+    def setup_class(cls):
+        cls.env = gym.make("MountainCarContinuous-v0")
+        cls.env = SparseRewardGoalEnv(cls.env)
+
+    def test_step(self):
+        obs, reward, done, info = self.env.step(self.env.action_space.sample())
+        self.assertTrue(not obs is None)
+        self.assertTrue(not reward is None)
+        self.assertTrue(reward in [0,1])
+
+    def test_reset(self):
+        state = self.env.reset()
+        self.assertTrue(isinstance(state, np.ndarray), "State is instance numpy array")
+        self.assertTrue(state.shape[0] == self.env.observation_space.shape[0], "State shape is equal to observation space shape")
+        self.assertTrue(not self.env.goal is None)
 
 
 if __name__ == '__main__':
