75a5955aec0a35f802bcbabd45915b38d8d6c5f0
Marin Vlastelica Pogancic <marin.vlastelicap@gmail.com>
2018-02-07 15:50:52 +0100

    Adapted example for ddpg

--- examples/ddpg.py
+++ examples/ddpg.py
@@ -1,12 +1,13 @@
 import gym
 from torch_rl.utils import *
-from collections import deque
 
 from torch_rl.models import SimpleNetwork
 from torch_rl.envs import NormalisedActionsWrapper
 from torch_rl.memory import SequentialMemory
 from torch_rl.training import DDPGTrainer
-
+from torch_rl.envs import SparseRewardGoalEnv
+from torch_rl.stats import RLTrainingStats
+import datetime
 """
     Implementation of deep deterministic policy gradients with soft updates.
 
@@ -29,9 +30,10 @@ weight_init_sigma = 0.003
 
 replay_memory = SequentialMemory(limit=6000000, window_length=1)
 
-env = NormalisedActionsWrapper(gym.make("Pendulum-v0"))
+env = SparseRewardGoalEnv(NormalisedActionsWrapper(gym.make("Pendulum-v0")))
+env.reset()
 num_actions = env.action_space.shape[0]
-num_observations = env.observation_space.shape[0]
+num_observations = env.observation_space.shape[0]*2
 relu, tanh = tor.nn.ReLU(), tor.nn.Tanh()
 
 actor = cuda_if_available(SimpleNetwork([num_observations, middle_layer_size[0], middle_layer_size[1], num_actions],
@@ -47,6 +49,9 @@ critic.apply(gauss_init(0, weight_init_sigma))
 # Training
 trainer = DDPGTrainer(env=env, actor=actor, critic=critic,
                       tau=tau, epsilon=epsilon, batch_size=batch_size, depsilon=depsilon, gamma=gamma,
-                      lr_actor=actor_learning_rate, lr_critic=critic_learning_rate, warmup=warmup
+                      lr_actor=actor_learning_rate, lr_critic=critic_learning_rate, warmup=warmup, replay_memory=replay_memory
                       )
-trainer.train(2000, max_episode_len=500, verbose=True)
+
+stats = RLTrainingStats(save_destination="/disk/no_backup/vlasteli/Projects/torch_rl/examples/ddpg_" + str(datetime.datetime.now()).replace(" ", "_"))
+
+trainer.train(2000, max_episode_len=500, verbose=True, callbacks=[stats])
