3745d9e676a0d94e7fe83e6a7d1f8e6fc4144d08
Marin Vlastelica Pogancic <marin.vlastelicap@gmail.com>
2018-02-20 13:23:32 +0100

    Added ParameterGrid test

---
+++ torch_rl/tests/param_tests.py
@@ -0,0 +1,29 @@
+from torch_rl.utils import ParameterGrid
+import pytest
+import unittest
+import sys
+
+class TestParameterGrid(unittest.TestCase):
+
+
+    def setup_class(cls):
+        cls.grid = ParameterGrid.from_config('grid.json')
+
+
+    def test_from_config(self):
+
+        grid = ParameterGrid.from_config('grid.json')
+
+
+    def test_iter(self):
+        for parameters in self.grid:
+    ***REMOVED***
+        assert False in self.grid.reservoir
+        assert 400 in self.grid.reservoir
+
+
+
+
+if __name__ == '__main__':
+    pytest.main([sys.argv[0]])
+
