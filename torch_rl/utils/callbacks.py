3f4602340c48569cfd9feaa671fc7a39d496747e
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2018-03-19 18:02:02 +0100

    Added callback for saving model checkpoints

---
+++ torch_rl/utils/callbacks.py
@@ -0,0 +1,52 @@
+from .utils import Callback, timestamp
+import os
+
+class CheckpointCallback(Callback):
+
+
+    def __init__(self, models=None, save_path=".", interval=1):
+        """
+        Init the object
+        :param models:      dict of name: model pairs, the names are used for saving
+                            the models.
+        :param save_path:   directory where all of the checkpoints are going to be stored in
+                            a tree hierarchy.
+        """
+
+        self.models = models
+        self.interval = interval
+        self.save_path = os.path.join(save_path, "checkpoints")
+
+        os.makedirs(save_path, exist_ok=True)
+
+        for k in self.models.keys():
+            model_dir = os.path.join(self.save_path, k)
+            os.makedirs(model_dir)
+
+    def step(self,  *args, **kwargs):
+
+        step = kwargs['step']
+        if step % self.interval == 0:
+            for name, model in self.models.items():
+                model.save(path=os.path.join(self.save_path, name, "{}_model.ckpt".format(timestamp())))
+
+
+    def episode_step(self, *args, **kwargs):
+
+        step = kwargs['episode']
+        if step % self.interval == 0:
+            for name, model in self.models.items():
+                model.save(path=os.path.join(self.save_path, name,  "{}_model.ckpt".format(timestamp())))
+
+
+
+
+
+
+
+
+
+
+
+
+
