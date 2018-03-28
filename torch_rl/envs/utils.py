9f99890e0adcc4b3e09db6069011c642219ec589
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2018-03-28 11:45:14 +0200

    Helper functions for printing environment info and determining if there is a potential goal in the observation space of the environment

--- torch_rl/envs/utils.py
+++ torch_rl/envs/utils.py
@@ -15,3 +15,40 @@ def wrapped_by(env, cls):
         env = env.env
         if isinstance(env, cls):
             return True
+
+
+def env_info(env):
+    spacing = 40
+    top = "#"*spacing, "ENV_INFO", "#"*spacing
+    bottom = "#"*len(top)
+    print(top)
+    print("Observation space high/low: ", env.observation_space.low, env.observation_space.high)
+    print("Action space high/low: ", env.observation_space.low, env.observation_space.high)
+    env.reset()
+    print("Few steps:")
+    for i in range(5):
+        print(env.step(env.action_space.sample()))
+    print(bottom)
+
+
+
+import numpy as np
+import roboschool
+import gym
+
+def potential_goal_indices(env):
+    """
+        Function does 100 environment steps and finds indices that dont change, those
+        are potential goals of the environment.
+    """
+
+    obs = env.reset()
+
+    #We should come to zero in the obs array on the end
+    for i in range(99):
+        obs_next = env.step(env.action_space.sample())[0]
+        obs = obs - obs_next if i%2 == 0 else obs + obs_next
+
+    return np.isclose(obs, 0, atol=1e-6)
+
+
