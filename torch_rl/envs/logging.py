6bbea5fdb442106240edcc904bb200587739204a
Marin Vlastelica Pogancic <marin.vlastelicap@gmail.com>
2018-03-30 13:57:57 +0200

    Keeping track of average episode reward within the wrapper

--- torch_rl/envs/logging.py
+++ torch_rl/envs/logging.py
@@ -7,25 +7,30 @@
 
 from torch_rl.utils import logger
 import gym
-
+from collections import deque
+import numpy as np
 
 class EnvLogger(gym.Wrapper):
 
-    def __init__(self, env, level=logger.INFO, track_attrs=['episode_reward']):
+    def __init__(self, env, level=logger.INFO, track_attrs=[]):
         super(EnvLogger, self).__init__(env)
         self.env = env
+        self.rdeque = deque(maxlen=100)
         self.level = level
-        self.obs, self.reward, self.info, self.done = None, None, None, None
+        self.obs, self.reward, self.info, self.done = None, None, None, False
         self.episode_reward = 0
         self.track_attrs = track_attrs
     
     def step(self, action):
-        self.obs, self.reward, self.info, self.done = self.env.step(action)
+        self.obs, self.reward, self.done, self.info = self.env.step(action)
         self.episode_reward += self.reward
         self.log()
         if self.done:
+            self.rdeque.append(self.episode_reward)
             self.episode_reward = 0
-        return  self.obs, self.reward, self.info, self.done
+        logger.logkv('avg_episode_reward', np.mean(self.rdeque))
+
+        return  self.obs, self.reward, self.done, self.info
 
     def log(self):
 
