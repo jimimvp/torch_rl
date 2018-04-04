2d44c95d8f6626adf31f72ad6f4836a527f71633
Marin Vlastelica Pogancic <marin.vlastelicap@gmail.com>
2018-04-04 18:50:36 +0200

    Typecasting to make check if everything is ok within the memory

--- torch_rl/memory/sequential.py
+++ torch_rl/memory/sequential.py
@@ -86,11 +86,11 @@ class SequentialMemory(Memory):
             terminal1_batch.append(0. if e.terminal1 else 1.)
 
         # Prepare and validate parameters.
-        state0_batch = np.array(state0_batch).reshape(batch_size, -1)
-        state1_batch = np.array(state1_batch).reshape(batch_size, -1)
-        terminal1_batch = np.array(terminal1_batch).reshape(batch_size, -1)
-        reward_batch = np.array(reward_batch).reshape(batch_size, -1)
-        action_batch = np.array(action_batch).reshape(batch_size, -1)
+        state0_batch = np.array(state0_batch, dtype=np.float32).reshape(batch_size, -1)
+        state1_batch = np.array(state1_batch, dtype=np.float32).reshape(batch_size, -1)
+        terminal1_batch = np.array(terminal1_batch, dtype=np.bool).reshape(batch_size, -1)
+        reward_batch = np.array(reward_batch, dtype=np.float32).reshape(batch_size, -1)
+        action_batch = np.array(action_batch, dtype=np.float32).reshape(batch_size, -1)
 
         if self.goals.length > 0:
             return state0_batch, goal_batch, action_batch, reward_batch, state1_batch, terminal1_batch
