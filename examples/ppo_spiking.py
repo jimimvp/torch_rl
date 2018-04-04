0144f32e464ce7cca4ced6b26d7d09a07d50e583
Marin Vlastelica Pogancic <marin.vlastelicap@gmail.com>
2018-04-04 18:50:10 +0200

    Added Proximal Policy Optimization with reservoir, for more stable training should interpolate policy gradients off- and on-policy

--- examples/ppo_spiking.py
+++ examples/ppo_spiking.py
@@ -23,7 +23,7 @@ import os
 
 
 config.set_root('torch_rl_ppo_spiking_example', force=True)
-config.configure_logging(clear=False, output_formats=['tensorboard', 'stdout'])
+config.configure_logging(clear=False, output_formats=['tensorboard', 'stdout', 'json'])
 # config.start_tensorboard()
 
 reservoir_size = 100
@@ -31,8 +31,8 @@ reservoir_size = 100
 env = EnvLogger(NormalisedActionsWrapper(gym.make('Pendulum-v0')))
 
 # Creating the reservoir for observation transformation
-reservoir = Reservoir(dt=0.03, sim_dt=0.01, input_size=env.observation_space.shape[0], network_size=reservoir_size, recursive=True,
-                 spectral_radius=1.,noise=False, synapse_filter=15e-4)
+reservoir = Reservoir(dt=0.01, sim_dt=0.005, input_size=env.observation_space.shape[0], network_size=reservoir_size, recursive=True,
+                 spectral_radius=.6,noise=False, synapse_filter=15e-3)
 
 # This wrapper transforms a normal observation to a spiking observation
 env = BaselinesNormalize(env)
@@ -46,7 +46,7 @@ with tor.cuda.device(0):
     network = ActorCriticPPO([reservoir_size, 64, 64, env.action_space.shape[0]])
     network.apply(xavier_uniform_init())
 
-    trainer = GPUPPOTrainer(network=network, env=env, n_update_steps=10, 
-        n_steps=2048, n_minibatches=32, lmda=.95, gamma=.99, lr=3e-4, epsilon=0.2, ent_coef=0.0)
+    trainer = GPUPPOTrainer(network=network, env=env, n_update_steps=5, 
+        n_steps=1024, n_minibatches=16, lmda=.95, gamma=.99, lr=3e-4, epsilon=0.1, ent_coef=0.0)
     trainer.train(horizon=100000, max_episode_len=500)
 
